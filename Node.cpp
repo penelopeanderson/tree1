9dcc67707762d2f574586277d107e609a562e01b
Penelope Anderson <penelope.a.anderson@vanderbilt.edu>
2023-03-10 13:21:48 -0600

    Test this out

---
+++ Node.cpp
@@ -0,0 +1,17 @@
+//
+// Created by penel on 3/7/2023.
+//
+
+#include "Node.h.tmp"
+
+using namespace std;
+
+template <typename T>
+Node<T>::Node(T item1, T item2) : first(item1), second(item2), left(nullptr), right(nullptr){
+
+}
+
+
+
+
+
